c9937038cce1ffdb675094af9d65370c526bc4b6
yaishenka <gagarinovdaniil@gmail.com>
2020-06-05 16:31:26 +0300

    add all; add merge all functional to travis

--- pdfcreator.py
+++ pdfcreator.py
@@ -1,13 +1,27 @@
-from PyPDF2 import PdfFileReader, PdfFileWriter
+from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
 from PIL import Image
 import os
 from io import BytesIO
 
-def compile_files():
-    sources_dir = 'src/'
-    compiled_dir = 'compiled/'
-    file_list = os.listdir(sources_dir)
+sources_dir = 'src/'
+compiled_dir = 'compiled/'
 
+def number_of_pdf(pdf):
+    return int(pdf.split(' ')[0])
+
+def merge_pdfs():
+    file_list = os.listdir(compiled_dir)
+    file_list.sort(key=number_of_pdf)
+    merger = PdfFileMerger()
+    for file in file_list:
+        if 'DS_Store' in file: # Костыль
+            continue
+        merger.append(compiled_dir + file) 
+    merger.write('all_files.pdf')
+    merger.close()    
+
+
+def compile_files():
     for task_number in file_list:
         if 'DS_Store' in task_number: # Костыль
             continue
@@ -25,10 +39,11 @@ def compile_files():
             new_task_file.addPage(PdfFileReader(buf).getPage(0))
         outputStream = open(compiled_dir + task_number  + ' билет'+ '.pdf', "wb")
         new_task_file.write(outputStream)
-        outputStream.close()
+        outputStream.close()    
 
 if os.environ.get('TRAVIS_COMMIT_MESSAGE') is None or 'Travis build' not in os.environ['TRAVIS_COMMIT_MESSAGE']:
     compile_files()
+    merge_pdfs()
 
 
 
